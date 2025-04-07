c451490cbc35fd60a01f2dc5c204318fb7c2a1fd
Volodymyr-AI <alternativeqoob@gmail.com>
2025-04-07 13:44:40 +0300

    Reworked main window to MVVM pattern

--- WPF_WinCompanion/MainWindow.xaml.cs
+++ WPF_WinCompanion/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using WPF_WinCompanion.Apps_Windows.FinanceTracker_App;
 using WPF_WinCompanion.Apps_Windows.Notes_App;
 using WPF_WinCompanion.Apps_Windows.Weather_App;
 using WPF_WinCompanion.Controls;
+using WPF_WinCompanion.ViewModel;
 
 namespace WPF_WinCompanion
 {
@@ -20,141 +21,30 @@ namespace WPF_WinCompanion
     /// </summary>
     public partial class MainWindow : Window
     {
-        private DispatcherTimer _timer;
-        //private NewsApiClient _newsApiClient;
-        
-        
         public MainWindow()
         {
             InitializeComponent();
-            StartDateTimeUpdater();
-            UpdateCurrencyValues();
-            //_newsApiClient = new NewsApiClient();
-            //LoadNews();
-            //SetWindowSize();
+            DataContext = new MainViewModel();
         }
 
-        private void SetWindowSize()
-        {
-            var screenWidth = SystemParameters.PrimaryScreenWidth;
-            var screenHeight = SystemParameters.PrimaryScreenHeight;
-
-            this.Width = screenWidth * 0.6;
-            this.Height = screenHeight * 0.4;
-        }
-
-        // Date and time widget
-        private void StartDateTimeUpdater()
-        {
-            _timer = new DispatcherTimer();
-            _timer.Interval = TimeSpan.FromSeconds(1);
-            _timer.Tick += Timer_Tick;
-            _timer.Start();
-        }
-
-        private void Timer_Tick(object sender, EventArgs e)
-        {
-            DateTimeText.Text = DateTime.Now.ToString("MMM dd  HH:mm:ss", CultureInfo.InvariantCulture);
-        }
-
-        // Currency widget
-        private async void UpdateCurrencyValues()
-        {
-            var rates = await GetExchangeRatesAsync();
-            if (rates != null)
-            {
-                DollarValue.Text = rates["USD"].ToString("F2");
-                EuroValue.Text = rates["EUR"].ToString("F2");
-                PoundValue.Text = rates["GBP"].ToString("F2");
-            }
-            else
-            {
-                MessageBox.Show("Failed to fetch rates");
-            }
-        }
-
-        private async Task<Dictionary<string, decimal>> GetExchangeRatesAsync()
-        {
-            string url = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchangenew?json";
-            using (HttpClient client = new HttpClient())
-            {
-                try
-                {
-                    string response = await client.GetStringAsync(url);
-                    var exchangeRates = JsonConvert.DeserializeObject<List<ExchangeRate>>(response);
-
-                    Dictionary<string, decimal> rates = new Dictionary<string, decimal>();
-                    foreach (var rate in exchangeRates)
-                    {
-                        if (rate.Cc == "USD" || rate.Cc == "EUR" || rate.Cc == "GBP")
-                        {
-                            rates[rate.Cc] = rate.Rate;
-                        }
-                    }
-                    return rates;
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show("Error fetching exchange rates: " + ex.Message);
-                    return null;
-                }
-            }
-        }
-
-
-        // Open apps' windows
         private void WeatherApp_Click(object sender, RoutedEventArgs e)
         {
-            WeatherWindow weatherWindow = new WeatherWindow();
-            weatherWindow.Show();
+            new WeatherWindow().Show();
         }
 
         private void FinanceTracker_Click(object sender, RoutedEventArgs e)
         {
-            FinanceTrackerWindow financeTrackerWindow = new FinanceTrackerWindow();
-            financeTrackerWindow.Show();
+            new FinanceTrackerWindow().Show();
         }
 
         private void NotesApp_Click(object sender, RoutedEventArgs e)
         {
-            NotesAppWindow notesappWindow = new NotesAppWindow();
-            notesappWindow.Show();
+            new NotesAppWindow().Show();
         }
 
         private void ChessApp_Click(object sender, RoutedEventArgs e)
         {
-            ChessWindow chessAppWindow = new ChessWindow();
-            chessAppWindow.Show();
+            new ChessWindow().Show();
         }
-
-        //News widget
-        //private async void LoadNews()
-        //{
-        //    var newsArticles = await _newsApiClient.GetNewsAsync();
-        //    if (newsArticles != null)
-        //    {
-        //        UpdateMarquee(newsArticles);
-        //    }
-        //    else
-        //    {
-        //        MessageBox.Show("Failed to fetch news. Please check the logs for details.");
-        //    }
-        //}
-
-        //private void UpdateMarquee(List<NewsArticle> newsArticles)
-        //{
-        //    if (newsArticles == null || newsArticles.Count == 0) return;
-
-        //    string newsText = string.Join(" | ", newsArticles.ConvertAll(article => article.Title));
-        //    MarqueeControl marqueeControl = (MarqueeControl)this.FindName("MarqueeControl");
-        //    if (marqueeControl != null)
-        //    {
-        //        marqueeControl.UpdateNewsText(newsText);
-        //    }
-        //    else
-        //    {
-        //        Console.WriteLine("MarqueeControl not found");
-        //    }
-        //}
     }
 }
---
+++ WPF_WinCompanion/ViewModel/MainViewModel.cs
@@ -0,0 +1,111 @@
+﻿using System.ComponentModel;
+using System.Globalization;
+using System.Net.Http;
+using System.Windows.Threading;
+using Currency_Exchange.Service.Domain;
+using Newtonsoft.Json;
+
+namespace WPF_WinCompanion.ViewModel;
+
+public class MainViewModel : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    private string _dateTimeText;
+    public string DateTimeText
+    {
+        get => _dateTimeText;
+        set
+        {
+            _dateTimeText = value;
+            OnPropertyChanged(nameof(DateTimeText));
+        }
+    }
+
+    private string _usdRate;
+    public string UsdRate
+    {
+        get => _usdRate;
+        set
+        {
+            _usdRate = value;
+            OnPropertyChanged(nameof(UsdRate));
+        }
+    }
+
+    private string _eurRate;
+    public string EurRate
+    {
+        get => _eurRate;
+        set
+        {
+            _eurRate = value;
+            OnPropertyChanged(nameof(EurRate));
+        }
+    }
+
+    private string _gbpRate;
+    public string GbpRate
+    {
+        get => _gbpRate;
+        set
+        {
+            _gbpRate = value;
+            OnPropertyChanged(nameof(GbpRate));
+        }
+    }
+
+    private DispatcherTimer _timer;
+
+    public MainViewModel()
+    {
+        StartClock();
+        _ = UpdateCurrencyValues();
+    }
+
+    private void StartClock()
+    {
+        _timer = new DispatcherTimer
+        {
+            Interval = TimeSpan.FromSeconds(1)
+        };
+        _timer.Tick += (s, e) =>
+        {
+            DateTimeText = DateTime.Now.ToString("MMM dd  HH:mm:ss", CultureInfo.InvariantCulture);
+        };
+        _timer.Start();
+    }
+
+    private async Task UpdateCurrencyValues()
+    {
+        var rates = await GetExchangeRatesAsync();
+        if (rates != null)
+        {
+            UsdRate = rates["USD"].ToString("F2");
+            EurRate = rates["EUR"].ToString("F2");
+            GbpRate = rates["GBP"].ToString("F2");
+        }
+    }
+
+    private async Task<Dictionary<string, decimal>> GetExchangeRatesAsync()
+    {
+        string url = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchangenew?json";
+        using HttpClient client = new HttpClient();
+        try
+        {
+            string response = await client.GetStringAsync(url);
+            var exchangeRates = JsonConvert.DeserializeObject<List<ExchangeRate>>(response);
+
+            return exchangeRates
+                .Where(x => x.Cc == "USD" || x.Cc == "EUR" || x.Cc == "GBP")
+                .ToDictionary(x => x.Cc, x => x.Rate);
+        }
+        catch
+        {
+            return null;
+        }
+    }
+
+    private void OnPropertyChanged(string propertyName)
+        => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+}
