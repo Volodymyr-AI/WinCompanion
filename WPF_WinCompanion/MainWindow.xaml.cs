309dfcfb8c96612a9fba8de27005f4c619651dbc
Volodymyr-AI <alternativeqoob@gmail.com>
2025-04-12 22:08:15 +0300

    Initial commit + closing all child windows after main window closed

--- WPF_WinCompanion/MainWindow.xaml.cs
+++ WPF_WinCompanion/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Markup;
 using WPF_WinCompanion.Apps_Windows.Chess_App.Views;
 using WPF_WinCompanion.Apps_Windows.FinanceTracker_App;
 using WPF_WinCompanion.Apps_Windows.Notes_App;
@@ -12,30 +13,56 @@ namespace WPF_WinCompanion
     /// </summary>
     public partial class MainWindow : Window
     {
+        
+        private List<Window> _childWindows = new List<Window>();
         public MainWindow()
         {
             InitializeComponent();
             DataContext = new MainViewModel();
+            this.Closed += MainWindow_Closed;
         }
 
+        private void MainWindow_Closed(object sender, EventArgs e)
+        {
+            foreach (var window in _childWindows)
+            {
+                window.Close();
+            }
+        }
+
+        private void AddChildWindow(Window window)
+        {
+            _childWindows.Add(window);
+            window.Closed += (s, e) => _childWindows.Remove(window);
+        }
+
+        
         private void WeatherApp_Click(object sender, RoutedEventArgs e)
         {
-            new WeatherWindow().Show();
+            var window = new WeatherWindow();
+            AddChildWindow(window);
+            window.Show();
         }
 
         private void FinanceTracker_Click(object sender, RoutedEventArgs e)
         {
-            new FinanceTrackerWindow().Show();
+            var window = new FinanceTrackerWindow();
+            AddChildWindow(window);
+            window.Show();
         }
 
         private void NotesApp_Click(object sender, RoutedEventArgs e)
         {
-            new NotesAppWindow().Show();
+            var window = new NotesAppWindow();
+            AddChildWindow(window);
+            window.Show();
         }
 
         private void ChessApp_Click(object sender, RoutedEventArgs e)
         {
-            new ChessWindow().Show();
+            var window = new ChessWindow();
+            AddChildWindow(window);
+            window.Show();
         }
     }
 }
