60b963eb32cddf67a52ff649386e5d8abcf4cc8b
Volodymyr-AI <alternativeqoob@gmail.com>
2025-05-15 20:42:05 +0300

    Created an advanced logic of disambiguation

--- ChessApp/Services/PieceNotationService/Notation/MoveNotationFormatter.cs
+++ ChessApp/Services/PieceNotationService/Notation/MoveNotationFormatter.cs
@@ -1,4 +1,5 @@
 using System.Numerics;
+using ChessApp.Models.Board;
 using ChessApp.Services.PieceNotationService.Entity;
 using ChessApp.Services.PieceNotationService.Notation.Types;
 using ChessApp.Services.PieceNotationService.Notation.Types.Interfaces;
@@ -9,14 +10,16 @@ public class MoveNotationFormatter : IMoveNotationFormatter
 {
     private readonly List<INotationPartProvider> _providers;
     private readonly ISuffixProvider _suffixProvider;
+    private readonly ChessBoardModel _board;
 
-    public MoveNotationFormatter()
+    public MoveNotationFormatter(ChessBoardModel board)
     {
+        _board = board;
         _providers = new List<INotationPartProvider>()
         {
             new CastleNotationProvider(),
             new PawnNotationProvider(),
-            new PieceNotationProvider(new DisambiguationService())
+            new PieceNotationProvider(new DisambiguationService(_board))
         };
         _suffixProvider = new SuffixProvider();
     }
